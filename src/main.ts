8e43535b2b3a3a20a84a9f8c856012f3f41032e3
Andrew Richardson <andrew.richardson@kaleido.io>
2021-08-04 11:28:25 -0400

    Import eslint/prettier settings
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap().catch(err => {
+  console.error(`Fatal error: ${err}`);
+});
