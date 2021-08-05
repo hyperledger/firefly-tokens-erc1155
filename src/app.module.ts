90fd152066badce20e04f4b2534bde26ff75a3a9
Andrew Richardson <andrew.richardson@kaleido.io>
2021-08-05 11:23:28 -0400

    Add ConfigService
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TokensModule } from './tokens/tokens.module';
 import { EventStreamModule } from './event-stream/event-stream.module';
 
 @Module({
-  imports: [TokensModule, EventStreamModule],
+  imports: [ConfigModule.forRoot(), TokensModule, EventStreamModule],
   controllers: [AppController],
   providers: [AppService],
 })
