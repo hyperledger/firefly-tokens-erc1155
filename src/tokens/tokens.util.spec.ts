8fd8b3b0c05a280fdbc084a46f4b3259fdbe8e68
Andrew Richardson <andrew.richardson@kaleido.io>
2021-08-10 10:09:28 -0400

    Move token utils into tokens module
    Signed-off-by: Andrew Richardson <andrew.richardson@kaleido.io>

--- src/tokens/tokens.util.spec.ts
+++ src/tokens/tokens.util.spec.ts
@@ -14,7 +14,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { packTokenId, packTokenUri, unpackTokenId, unpackTokenUri } from './util';
+import { packTokenId, packTokenUri, unpackTokenId, unpackTokenUri } from './tokens.util';
 
 describe('Util', () => {
   it('packTokenUri', () => {
