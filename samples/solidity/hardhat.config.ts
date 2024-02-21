9bf89bedae2433f73a77ae492cd5508dac45f341
Nicko Guyer <nicko.guyer@kaleido.io>
2024-02-21 15:00:36 -0500

    Update smart contracct dev dependencies
    Signed-off-by: Nicko Guyer <nicko.guyer@kaleido.io>

--- samples/solidity/hardhat.config.ts
+++ samples/solidity/hardhat.config.ts
@@ -1,16 +1,12 @@
 import * as dotenv from "dotenv";
 
-import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "hardhat-gas-reporter";
-import "solidity-coverage";
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
 
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: "0.8.24",
   networks: {
     ropsten: {
       url: process.env.ROPSTEN_URL || "",
